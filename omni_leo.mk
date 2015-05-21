4e9ee1c736b05bd14125bb282d3b71334306dabc
David Vaz Guijarro <vazdavid@icloud.com>
2015-05-22 00:03:52 +0200

    Blobs

--- omni_leo.mk
+++ omni_leo.mk
@@ -27,11 +27,15 @@ DEVICE_PACKAGE_OVERLAYS += \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product, device/sony/shinano-common/device.mk)
-$(call inherit-product, vendor/sony/leo/leo-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 $(call inherit-product-if-exists, prebuilts/chromium/webview_prebuilt.mk)
 $(call inherit-product-if-exists, vendor/google/products/gms.mk)
+
+# Include non-opensource parts
+$(call inherit-product, vendor/sony/shinano-common/shinano-common-vendor.mk)
+$(call inherit-product, vendor/sony/msm8974-common/msm8974-common-vendor.mk)
+$(call inherit-product, vendor/sony/z3/z3-vendor.mk)
 
 PRODUCT_COPY_FILES += \
     device/sony/leo/rootdir/system/etc/BCM4339.hcd:system/etc/firmware/BCM43xx.hcd \
